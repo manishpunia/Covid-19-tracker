5feb4cd82aa12bc9c9e214b00adae2811425a1ae
manishpunia <71880981+manishpunia@users.noreply.github.com>
2024-01-09 00:34:12 +0530

    Add files via upload

---
+++ InfoBox.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./InfoBox.css";
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+function InfoBox({ title, cases, active, total, ...props }) {
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"}`}
+    >
+      <CardContent>
+        <Typography className="infoBox__title" color="black">
+          <strong> {title}</strong>
+        </Typography>
+        <h2 className="infoBox__cases">{cases}</h2>
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
